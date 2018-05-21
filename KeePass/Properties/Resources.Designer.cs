f021df3c254420ae1b0b5f6a7df30a72a2756230
Daniel <dhcgn@secure.mailbox.org>
2018-05-21 19:14:52 +0200

    KeePass 2.39

--- KeePass/Properties/Resources.Designer.cs
+++ KeePass/Properties/Resources.Designer.cs
@@ -653,6 +653,16 @@ namespace KeePass.Properties {
         /// <summary>
         ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap B16x16_Imp_Chrome {
+            get {
+                object obj = ResourceManager.GetObject("B16x16_Imp_Chrome", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap B16x16_Imp_CWallet {
             get {
                 object obj = ResourceManager.GetObject("B16x16_Imp_CWallet", resourceCulture);
